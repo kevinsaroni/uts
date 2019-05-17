e34aec36db42f2dfda0a17d83df3542ba65b7fbe
septianjodhi <48472851+septianjodhi@users.noreply.github.com>
2019-05-17 13:02:04 +0700

    Update insert.php

--- www/insert.php
+++ www/insert.php
@@ -2,10 +2,13 @@
  include "db.php";
  if(isset($_POST['insert']))
  {
- $nama_lk=$_POST['nama_lk'];
- $nim=$_POST['nim'];
- $nama_anggota=$_POST['nama_anggota'];
- $q=mysqli_query($con,"INSERT INTO `data` (`nama_lk`,`nim`,`nama_anggota`) VALUES ('$nama_lk','$nim','$nama_anggota')");
+ $nama_lengkap=$_POST['nama_lengkap'];
+ $NIK=$_POST['NIK'];
+ $jenis_kelamin=$_POST['jenis_kelamin'];
+ $TTL=$_POST['TTL'];
+ $agama=$_POST['agama'];
+ $pekerjaan=$_POST['pekerjaan'];
+ $q=mysqli_query($con,"INSERT INTO `data` (`nama_lengkap`,`NIK`,`jenis_kelamin`,`TTL`,`agama`,`pekerjaan`) VALUES ('$nama_lengkap','$NIK','$jenis_kelamin','$TTL','$agama','$pekerjaan')");
  if($q)
   echo "success";
  else
