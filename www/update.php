18b5178965fa28b73121f52ab969016b245511bf
septianjodhi <48472851+septianjodhi@users.noreply.github.com>
2019-05-17 13:04:10 +0700

    Update update.php

--- www/update.php
+++ www/update.php
@@ -3,10 +3,13 @@
  if(isset($_POST['update']))
  {
  $id=$_POST['id'];
- $nama_lk=$_POST['nama_lk'];
- $nim=$_POST['nim'];
- $nama_anggota=$_POST['nama_anggota'];
- $q=mysqli_query($con,"UPDATE `data` SET `nama_lk`='$nama_lk',`nim`='$nim',`nama_anggota`='$nama_anggota' where `id`='$id'");
+ $nama_lengkap=$_POST['nama_lengkap'];
+ $NIK=$_POST['NIK'];
+ $jenis_kelamin=$_POST['jenis_kelamin'];
+ $TTL=$_POST['TTL'];
+ $agama=$_POST['agama'];
+ $pekerjaan=$_POST['pekerjaan'];
+ $q=mysqli_query($con,"UPDATE `data` SET `nama_lengkap`='$nama_lengkap',`NIK`='$NIK',`jenis_kelamin`='$jenis_kelamin',`TTL`='$TTL',`agama`='$agama',`pekerjaan`='$pekerjaan' where `id`='$id'");
  if($q)
  echo "success";
  else
