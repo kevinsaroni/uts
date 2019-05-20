e9e3302c7a88d7789255955bf4b140d69bb78fd5
kevinsaroni <48478174+kevinsaroni@users.noreply.github.com>
2019-05-20 14:13:48 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id9594042_cekdata","12345678","id9594042_localhost") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id8952319_petruk","kevin198","localhost") or die ("could not connect database");
 ?>
